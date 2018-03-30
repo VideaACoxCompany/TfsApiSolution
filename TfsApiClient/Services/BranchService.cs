bf81be6ac3ddc414defa662121ac4717f7dafc17
Chu, Johnny (CMG-Videa) <Johnny.Chu@Videa.tv>
2018-03-29 23:53:31 -0400

    added html and style

--- TfsApiClient/Services/BranchService.cs
+++ TfsApiClient/Services/BranchService.cs
@@ -31,17 +31,49 @@ namespace TfsApiClient.Services
                 var devReleaseBranches = new[] {"dev", "release"};
 
                 var result = new StringBuilder();
+                /*
+                 * 
+                 * <table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+   <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+</table>
+*/
+
+                result.AppendLine(@"<style>
+                    table, th, td {
+                        border: 1px solid black;
+                        border-collapse: collapse;
+                    }
+                    th, td {
+                        padding: 5px;
+                    }
+                    th, td {
+                        text-align: center;
+                    }
+                    ");
+                result.AppendLine("<table>");
+                result.AppendLine("<tr><th>Repo</th><th>Branch</th><th>Behind|Ahead master</th></tr>");
 
                 foreach (var repo in repos)
                 {
                     var gitStat = await gitStatService.GetResult(repo.Key);
                     var devReleaseBranchesStat = gitStat.Value.Where(i => devReleaseBranches.Contains(i.Name)).ToList();
-
+                    
                     devReleaseBranchesStat.ForEach(i =>
                         result.AppendLine(
-                            $"Repo: {repo.Value} Branch {i.Name} is {i.BehindCount} commit(s) behind and {i.AheadCount} commit(s) ahead master branch <br/> "));
-
+                            $"<tr > <td> {repo.Value} </td> <td> {i.Name} </td> <td> {i.BehindCount} | {i.AheadCount} </td> </tr> "));
+                    
                 }
+                result.AppendLine("</table>");
+
                 return result.ToString();
 
             }
